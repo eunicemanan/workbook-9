ee262e01d536ce5ecef9126bad6110371b29dd03
Eunice_Manan <mananeunice@gmail.com>
2025-06-17 09:39:42 -0400

    PersonDao, BuildingDao in progress

---
+++ first-spring-app/src/main/java/com/example/first_spring_app/ConsoleApplication.java
@@ -0,0 +1,21 @@
+package com.example.first_spring_app;
+
+import com.example.first_spring_app.model.Person;
+import com.example.first_spring_app.repository.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsoleApplication {
+
+    @Autowired
+    PersonDAO personDAO;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Person person = personDAO.find();
+        System.out.println(person);
+
+    }
+
+}
---
+++ first-spring-app/src/main/java/com/example/first_spring_app/repository/BuildingDAO.java
@@ -0,0 +1,7 @@
+package com.example.first_spring_app.repository;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BuildingDAO {
+}
